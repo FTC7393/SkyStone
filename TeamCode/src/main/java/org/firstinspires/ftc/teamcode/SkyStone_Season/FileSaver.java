ae3adc74ecc66acd33d911f24a65c9faa84d41dd
jvandegriff <jon.vandegriff@jhuapl.edu>
2019-11-13 20:06:06 -0500

    updated hist method to move Mat creation out of loops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/FileSaver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/FileSaver.java
@@ -19,6 +19,12 @@ public class FileSaver {
     boolean isBusy = false;
     private int fileIndexCounter = 0;
 
+    private final MatOfFloat ranges = new MatOfFloat(); //0.0f);
+    private final Mat histResult = new Mat(25);
+    private final MatOfInt channels = new MatOfInt(0);
+    private final Mat mask = new Mat();
+    private final MatOfInt histSize = new MatOfInt(25);
+
     // suggested dirname is "/sdcard/FIRST/histograms"
     public FileSaver(String phoneSaveDir) {
         this.phoneSaveDir = phoneSaveDir;
@@ -32,21 +38,16 @@ public class FileSaver {
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                MatOfFloat ranges = new MatOfFloat(); //0.0f);
-                Mat histResult = new Mat(25);
-                MatOfInt channels = new MatOfInt(0);
-                Mat mask = new Mat();
-                MatOfInt histSize = new MatOfInt(25);
                 List<Mat> images = new ArrayList<>();
                 images.add(image);
-//                Imgproc.calcHist(images, channels, mask, histResult, histSize, ranges);
-//                // we expect histResult to have a size of "w" for number of x pixels, and a height of 1 (only got one channel!)
-//                String filename =
-//                        String.format("%s/%s_%04d.png", phoneSaveDir, filenameBase, fileIndexCounter++);
-//
-//                File dir = FileUtil.getDir(phoneSaveDir);
-//
-//                Imgcodecs.imwrite(filename, histResult);
+                Imgproc.calcHist(images, channels, mask, histResult, histSize, ranges);
+                // we expect histResult to have a size of "w" for number of x pixels, and a height of 1 (only got one channel!)
+                String filename =
+                        String.format("%s/%s_%04d.png", phoneSaveDir, filenameBase, fileIndexCounter++);
+
+                File dir = FileUtil.getDir(phoneSaveDir);
+
+                Imgcodecs.imwrite(filename, histResult);
                 isBusy = false;
             }
         };
