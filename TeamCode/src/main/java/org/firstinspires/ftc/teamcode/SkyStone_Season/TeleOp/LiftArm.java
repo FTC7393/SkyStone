5eb24bdb63abb7fe03a0c4939aa463175ce7c982
SriTree <kssrivatsaan@gmail.com>
2019-11-23 13:40:26 -0500

    cleaning up main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/TeleOp/LiftArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/TeleOp/LiftArm.java
@@ -11,29 +11,42 @@ public class LiftArm {
 
     private ServoControl elbow, wrist, fingers;
 
-    public LiftArm(ServoControl elbow, ServoControl wrist, ServoControl fingers){
+    public LiftArm(ServoControl elbow, ServoControl wrist, ServoControl fingers) {
         this.elbow = elbow;
         this.wrist = wrist;
         this.fingers = fingers;
     }
 
-    public void Extend() {
+    public void extend() {
         elbow.goToPreset(SkystoneRobotCfg.ElbowServoPresets.EXTEND);
         wrist.goToPreset(SkystoneRobotCfg.WristServoPresets.EXTEND);
 
     }
 
-    public void Retract() {
+    public void retract() {
         elbow.goToPreset(SkystoneRobotCfg.ElbowServoPresets.RETRACT);
         wrist.goToPreset(SkystoneRobotCfg.WristServoPresets.RETRACT);
     }
 
-    public void Grab() {
+    public void right() {
+        wrist.goToPreset(SkystoneRobotCfg.WristServoPresets.RIGHT);
+    }
+
+    public void left() {
+        wrist.goToPreset(SkystoneRobotCfg.WristServoPresets.LEFT);
+    }
+
+    public void grab() {
         fingers.goToPreset(SkystoneRobotCfg.FingersServoPresets.GRAB);
     }
 
-    public void Release() {
-        fingers.goToPreset(SkystoneRobotCfg.FingersServoPresets.RELEASE);
+    public void release() { fingers.goToPreset(SkystoneRobotCfg.FingersServoPresets.RELEASE); }
+
+
+    public void act() {
+        elbow.act();
+        wrist.act();
+        fingers.act();
     }
 
 }
