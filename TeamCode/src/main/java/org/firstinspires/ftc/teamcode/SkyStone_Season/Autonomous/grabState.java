0613e68a9c6f766ac732fe0d59c8ce7a6dac6e65
Abhi Divvela <saidivvela2005@gmail.com>
2019-11-26 21:52:19 -0500

    continued state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/Autonomous/grabState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone_Season/Autonomous/grabState.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.SkyStone_Season.Autonomous;
 
+import org.firstinspires.ftc.teamcode.SkyStone_Season.SkystoneRobotCfg;
+import org.firstinspires.ftc.teamcode.SkyStone_Season.TeleOp.FlyWheels;
+
 import ftc.electronvolts.statemachine.State;
 import ftc.electronvolts.statemachine.StateName;
 
 public class grabState implements State {
+    FlyWheels flywheels;
+    SkystoneRobotCfg robotCfg;
+    boolean isFirst = true;
     @Override
     public StateName act() {
+        flywheels = robotCfg.getFlyWheels();
+        if (isFirst) {
+            isFirst = false;
+
+        }
         return null;
     }
 }
